348281e3636b1a74510e876b3e31537de998b66b
8696 <204084802@qq.com>
2020-05-08 16:28:25 +0800

    Fri May 08 2020 16:28:25 GMT+0800 (GMT+08:00)

--- git.push.js
+++ git.push.js
@@ -3,7 +3,7 @@ const {execSync} = require('child_process');
 
 execSync('git add .');
 
-execSync('git commit -m "' + new Date() + '"');
+execSync('git commit -m "' + new Date().toString() + '"');
 
 execSync('git push origin dev');
 
