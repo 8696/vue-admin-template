617e61343a4c35f9dddcbd847415da834eee3fb6
8696 <204084802@qq.com>
2020-06-04 17:48:14 +0800

    Thu Jun 04 2020 17:48:14 GMT+0800 (China Standard Time)

--- src/api/utils.js
+++ src/api/utils.js
@@ -2,7 +2,8 @@ import qs from 'qs';
 
 /**
  * @deprecated 将对象转成 FormData
- * @param object {FormData}
+ * @param object {Object}
+ * @return {FormData}
  * */
 export function makeFormData (object) {
   let formData = new FormData();
@@ -15,6 +16,7 @@ export function makeFormData (object) {
 /**
  * @deprecated 将对象转成 query string
  * @param object {Object}
+ * @return {String}
  * */
 export function makeQueryString (object) {
   return qs.stringify(object);
